59d47f2cfdba662a85d5b84ad659c38b763dbf6f
Samuel Gagnon <gagnons.infynox@gmail.com>
2020-07-13 11:13:48 -0400

    Create Script_PS_Windows_FindAndUninstallSoftware.ps1

---
+++ PowerShell/Script_PS_Windows_FindAndUninstallSoftware.ps1
@@ -0,0 +1,27 @@
+﻿$Global:Output = @()
+
+$ApplicationName = '$env:SoftwareName'
+$Selection = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | 
+ForEach-Object { Get-ItemProperty $_.PSPath } |
+Where-Object { $_.DisplayName -match "$ApplicationName" } |
+Select-Object DisplayName,UninstallString
+
+$Selection | ForEach-Object {
+    $command = 'cmd.exe /C ' + '"' + $_.UninstallString + ' /qn"'
+    $command = $command -replace "/I{", "/x{"
+    Return $command
+    Invoke-Expression -Command $command
+}
+
+$Selection2 = Get-ChildItem "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | 
+ForEach-Object { Get-ItemProperty $_.PSPath } |
+Where-Object { $_.DisplayName -match "$ApplicationName" } |
+Select-Object DisplayName,UninstallString
+
+
+$Selection2 | ForEach-Object {
+    $command = 'cmd.exe /C ' + '"' + $_.UninstallString + ' /qn"'
+    $command = $command -replace "/I{", "/x{"
+    Return $command
+    Invoke-Expression -Command $command
+}
