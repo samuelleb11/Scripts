7310c38d10125a20dc5828ae7cf237c318890740
Samuel Gagnon <gagnons.infynox@gmail.com>
2021-01-30 23:22:19 -0500

    Create Script_PS_Windows_Ping.ps1

---
+++ PowerShell/Script_PS_Windows_Ping.ps1
@@ -0,0 +1,13 @@
+﻿$Data = @()
+$Times = 10
+$Count = 0
+
+$ping = New-Object System.Net.NetworkInformation.Ping
+
+while($Count -le $Times)
+{
+    $data += $ping.Send('192.46.223.26')
+    $Count ++
+    Start-Sleep -Seconds 1
+}
+$Data
