b19192daa5a6682226b1e5aab6d8ab2243dd1180
Samuel Gagnon <gagnons.infynox@gmail.com>
2021-02-01 09:27:19 -0500

    Update Script_PS_Windows_LogonLogoffEventsHistory.ps1

--- PowerShell/Script_PS_Windows_LogonLogoffEventsHistory.ps1
+++ PowerShell/Script_PS_Windows_LogonLogoffEventsHistory.ps1
@@ -1,3 +1,24 @@
 ﻿$logs = get-eventlog system -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-31);
 $res = @(); ForEach ($log in $logs) {if($log.instanceid -eq 7001) {$type = "Logon"} Elseif ($log.instanceid -eq 7002){$type="Logoff"} Else {Continue} $res += New-Object PSObject -Property @{Time = $log.TimeWritten; "Event" = $type; User = (New-Object System.Security.Principal.SecurityIdentifier $Log.ReplacementStrings[1]).Translate([System.Security.Principal.NTAccount])}};
 $res | Export-Csv -Path C:\temp\LogonLogs.csv -NoTypeInformation
+
+#RDP
+$LogName = 'Microsoft-Windows-TerminalServices-LocalSessionManager/Operational'
+$Results = @()
+$Events = Get-WinEvent -LogName $LogName
+foreach ($Event in $Events) {
+    $EventXml = [xml]$Event.ToXML()
+
+    $ResultHash = @{
+        Time        = $Event.TimeCreated.ToString()
+        'Event ID'  = $Event.Id
+        'Desc'      = ($Event.Message -split "`n")[0]
+        Username    = $EventXml.Event.UserData.EventXML.User
+        'Source IP' = $EventXml.Event.UserData.EventXML.Address
+        'Details'   = $Event.Message
+    }
+
+    $Results += (New-Object PSObject -Property $ResultHash)
+}
+
+$Results | Export-Csv -Path C:\temp\RDPLogs.csv -NoTypeInformation
